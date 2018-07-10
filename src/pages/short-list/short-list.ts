c94bf0bb78e705d9eb2956cc61a679cb62fa69c5
Noven Purnell-Webb <noven@liberant.com.au>
2018-07-11 03:05:54 +1000

    recover

--- src/pages/short-list/short-list.ts
+++ src/pages/short-list/short-list.ts
@@ -1,12 +1,10 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {IonicPage, NavController, NavParams, ViewController, ToastController} from 'ionic-angular';
+import {Observable} from 'rxjs/Observable';
+import {FirestoreProvider} from '../../providers/firestore/firestore';
+import {Product} from '../../models/product-model';
 
-/**
- * Generated class for the ShortListPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+
 
 @IonicPage()
 @Component({
@@ -14,12 +12,79 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'short-list.html',
 })
 export class ShortListPage {
+  public productsList: Observable<Product[]>;
+  public busId: string;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private afs: FirestoreProvider, public viewCtrl: ViewController, public toastCtrl: ToastController) {
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad ShortListPage');
+    /*this.afs.getBusId().then(busId => {
+      this.busId = busId;
+      this.productsList = this.afs.col$<Product>('business/' + busId + '/' + this.listType)
+    });
+    */
+   this.getProducts();
   }
+
+  async getProducts(){
+    this.busId = await this.afs.getBusId();
+    this.productsList = await this.afs.col$<Product>(`business/${this.busId}/shortlist`);
+
+  }
+
+  /*createItem(product: Product): FormArray {
+    return this.fb.array({
+    id: product.id,
+    pid: product.pid,
+    name: product.name,
+    producer: product.producer,
+    brand: product.brand,
+    vintage: product.vintage,
+    region: product.region,
+    variety: product.variety,
+    photoURL: product.photoURL,
+    cartonSize: product.cartonSize,
+    active: product.active,
+    qty: product.qty,
+    price: product.cartonSize * product.unitCost,
+    });
+  } */
+
+
+  detail(id: string) {
+    this.navCtrl.push('ProductPage', {id: id});
+  }
+
+  closeModal() {
+    this.viewCtrl.dismiss();
+  }
+
+
+  async addList(product) {
+    let res = await this.afs.upsert(`business/${this.busId}/winelist/${product.id}`, product)
+      this.presentToast('Wine added successfully');
+      console.log(res);
+    this.afs.delete(`business/${this.busId}/shortlist/${product.id}`);
+  }
+
+  contact(id){
+    console.log(id);
+  }
+
+  presentToast(message) {
+    let toast = this.toastCtrl.create({
+      message: message,
+      duration: 3000,
+      position: 'top'
+    });
+
+    toast.onDidDismiss(() => {
+      console.log('Dismissed toast');
+    });
+
+    toast.present();
+  }
+
 
 }
--- src/pages/wine-list/wine-list.ts
+++ src/pages/wine-list/wine-list.ts
@@ -1,10 +1,10 @@
 import {Component} from '@angular/core';
 import {IonicPage, NavController, NavParams, ViewController, ToastController} from 'ionic-angular';
+import { FormBuilder, FormGroup, FormArray } from '@angular/forms';
 import {Observable} from 'rxjs/Observable';
 import {FirestoreProvider} from '../../providers/firestore/firestore';
 import { OrdersProvider} from "../../providers/orders/orders";
 import {Product} from '../../models/product-model';
-
 
 @IonicPage()
 @Component({
@@ -15,18 +15,80 @@ export class WineListPage {
   public productsList: Observable<Product[]>;
   public busId: string;
   public listType: string;
+  public orderForm: FormGroup;
+  //public prods: FormArray;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private afs: FirestoreProvider, public viewCtrl: ViewController, public toastCtrl: ToastController, public op: OrdersProvider ) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private afs: FirestoreProvider, public viewCtrl: ViewController, public toastCtrl: ToastController, public op: OrdersProvider, private fb: FormBuilder ) {
     this.listType = navParams.get('type');
+    if (this.listType === 'winelist'){this.initForm();}
   }
 
   ionViewDidLoad() {
     console.log(this.listType);
-    this.afs.getBusId().then(busId => {
+    /*this.afs.getBusId().then(busId => {
       this.busId = busId;
-      this.productsList = this.afs.colWithIds$<Product>('business/' + busId + '/' + this.listType);
+      this.productsList = this.afs.col$<Product>('business/' + busId + '/' + this.listType)
+    });
+    */
+   this.getProducts();
+  }
+
+  async getProducts(){
+    this.busId = await this.afs.getBusId();
+    this.productsList = await this.afs.col$<Product>(`business/${this.busId}/${this.listType}`);
+    if (this.listType === 'winelist'){
+      this.initProducts();
+      }
+  }
+
+  /*createItem(product: Product): FormArray {
+    return this.fb.array({
+    id: product.id,
+    pid: product.pid,
+    name: product.name,
+    producer: product.producer,
+    brand: product.brand,
+    vintage: product.vintage,
+    region: product.region,
+    variety: product.variety,
+    photoURL: product.photoURL,
+    cartonSize: product.cartonSize,
+    active: product.active,
+    qty: product.qty,
+    price: product.cartonSize * product.unitCost,
+    });
+  } */
+
+  initForm() {
+    this.orderForm = this.fb.group({
+      rid: this.busId,
+      total: [''],
+      prods: this.fb.array([ this.initProductControl(), ]),
     });
   }
+
+  initProducts() {
+  this.productsList.subscribe((_products) => {
+    console.log(_products);
+    let chosenProducts = [];
+    _products.forEach(product => {
+      const control = <FormArray>this.orderForm.controls['prods'];
+      control.push(
+        this.fb.group({
+        name: product.name,
+        qty: product.qty || 0,
+      })
+      )
+      });
+    });
+  }
+  initProductControl() {
+    return this.fb.group({
+      name: [''],
+      qty: [''],
+    });
+  } 
+
 
   detail(id: string) {
     this.navCtrl.push('ProductPage', {id: id});
@@ -38,7 +100,7 @@ export class WineListPage {
 
   async placeOrder(product: Product): Promise<any> {
     await this.op.placeOrder(product);
-    this.presentToast(product.qty +  ' bottles of ' + product.name + ' ordered');
+    this.presentToast(`${product.qty} bottles of ${product.name} ordered`);
   }
 
 
